dbbb304b8b7ec9f921661bc9e2b7b7723a433b51
Charlie Gaines <beaugaines@gmail.com>
2014-12-15 11:36:14 -0500

    various

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  authenticated :user do
+    root to: 'dashboards#show', as: :authenticated_root
+  end
+  
   root :to => "welcome#index"
 end
